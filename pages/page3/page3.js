a4a2bf574247bbb061d4e50c3721454da4ae1fba
Jacka10086 <496806255@qq.com>
2024-07-03 16:15:41 +0800

    弹出层实现

--- pages/page3/page3.js
+++ pages/page3/page3.js
@@ -6,40 +6,50 @@ Page({
     gender: "",
     province: "",
     city: "",
-    country: ""
+    country: "",
+    showEditSheet: false,
+    actions: [{ name: '保存', color: '#07c160' }],
+    editNickName: '',
+    editGender: '',
+    editProvince: '',
+    editCity: ''
   },
 
-  // 自动获取用户信息
-  onLoad: function (options) {
+  onLoad: function(options) {
     var that = this;
     wx.getUserInfo({
       success: function(res) {
         console.log(res);
-        
-        var userInfo = res.userInfo
+
+        var userInfo = res.userInfo;
         console.log(userInfo);
-        var nickName = userInfo.nickName
-        var avatarUrl = userInfo.avatarUrl
-        var gender = userInfo.gender  //性别 0：未知、1：男、2：女
-        var province = userInfo.province
-        var city = userInfo.city
-        var country = userInfo.country
+        var nickName = userInfo.nickName;
+        var avatarUrl = userInfo.avatarUrl;
+        var gender = userInfo.gender; // 性别 0：未知、1：男、2：女
+        var province = userInfo.province;
+        var city = userInfo.city;
+        var country = userInfo.country;
         if (gender == 1) {
-          gender = '男'
+          gender = '男';
         } else if (gender == 2) {
-          gender = '女'
+          gender = '女';
         } else {
-          gender = '未知'
+          gender = '未知';
         }
         that.setData({
           nickName: nickName,
           avatarUrl: avatarUrl,
           gender: gender,
           country: country,
-          province: province
-        })
+          province: province,
+          city: city,
+          editNickName: nickName,
+          editGender: gender,
+          editProvince: province,
+          editCity: city
+        });
       }
-    })
+    });
   },
 
   onShow() {
@@ -71,5 +81,39 @@ Page({
 
   navigateToPage3() {
     wx.redirectTo({ url: '/pages/page3/page3' });
+  },
+
+  onEditTap() {
+    this.setData({ showEditSheet: true });
+  },
+
+  onClose() {
+    this.setData({ showEditSheet: false });
+  },
+
+  onSelect() {
+    this.setData({
+      nickName: this.data.editNickName,
+      gender: this.data.editGender,
+      province: this.data.editProvince,
+      city: this.data.editCity,
+      showEditSheet: false
+    });
+  },
+
+  onNickNameChange(event) {
+    this.setData({ editNickName: event.detail });
+  },
+
+  onGenderChange(event) {
+    this.setData({ editGender: event.detail });
+  },
+
+  onProvinceChange(event) {
+    this.setData({ editProvince: event.detail });
+  },
+
+  onCityChange(event) {
+    this.setData({ editCity: event.detail });
   }
 });

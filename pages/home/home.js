5f0da087af0fdc5fdeea9bedf65366b49c4172e8
Jacka10086 <496806255@qq.com>
2024-07-03 14:30:56 +0800

    2

--- pages/home/home.js
+++ pages/home/home.js
@@ -4,7 +4,11 @@ Page({
     activeTab: 0,
     devices: [],
     category1Devices: [],
-    category2Devices: []
+    category2Devices: [],
+    touchStartX: 0,
+    touchStartY: 0,
+    touchEndX: 0,
+    touchEndY: 0
   },
 
   onLoad() {
@@ -49,6 +53,10 @@ Page({
     wx.redirectTo({ url: '/pages/page3/page3' });
   },
 
+  onTabChange(event) {
+    this.setData({ activeTab: event.detail.index });
+  },
+
   onSwitchChange(event) {
     const { id } = event.currentTarget.dataset;
     const { devices, category1Devices, category2Devices } = this.data;
@@ -68,5 +76,32 @@ Page({
       category1Devices: updatedCategory1Devices,
       category2Devices: updatedCategory2Devices
     });
+  },
+
+  onTouchStart(event) {
+    this.setData({
+      touchStartX: event.touches[0].clientX,
+      touchStartY: event.touches[0].clientY
+    });
+  },
+
+  onTouchMove(event) {
+    this.setData({
+      touchEndX: event.touches[0].clientX,
+      touchEndY: event.touches[0].clientY
+    });
+  },
+
+  onTouchEnd(event) {
+    const deltaX = this.data.touchEndX - this.data.touchStartX;
+    const deltaY = this.data.touchEndY - this.data.touchStartY;
+
+    if (Math.abs(deltaX) > Math.abs(deltaY)) {
+      if (deltaX > 5 && this.data.activeTab > 0) {
+        this.setData({ activeTab: this.data.activeTab - 1 });
+      } else if (deltaX < -5 && this.data.activeTab < 2) {
+        this.setData({ activeTab: this.data.activeTab + 1 });
+      }
+    }
   }
 });

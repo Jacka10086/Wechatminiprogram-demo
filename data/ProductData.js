696e0bdaf6ab44751c85287e503bb3b8e0f43a9e
Jacka10086 <496806255@qq.com>
2024-07-05 17:13:27 +0800

    2

--- data/ProductData.js
+++ data/ProductData.js
@@ -7,8 +7,8 @@ module.exports = {
     { src: 'https://www.leezon.net/uploads/news/20200226150155588600.jpg' }
   ],
   imageItems: [
-    { src: 'https://www.leezon.net/uploads/news/20200226150155588600.jpg', text: '1' },
-    { src: 'https://www.leezon.net/uploads/news/20200226150155588600.jpg', text: '2' },
+    { src: 'https://img.alicdn.com/bao/uploaded/i2/2371762012/O1CN01dG70E11QjXq71WXup_!!0-item_pic.jpg', text: '1' },
+    { src: 'https://img.alicdn.com/bao/uploaded/i3/2371762012/O1CN01SBW8af1QjXopdRW2W_!!0-item_pic.jpg', text: '2' },
     { src: 'https://www.leezon.net/uploads/news/20200226150155588600.jpg', text: '3' }
   ]
 };

28e4b6a598fb665a4292ff9cd445efff739413d7
Jacka10086 <496806255@qq.com>
2024-07-05 15:30:14 +0800

    userinfo page storage fix

--- pages/page3/page3.js
+++ pages/page3/page3.js
@@ -16,40 +16,7 @@ Page({
   },
 
   onLoad: function(options) {
-    var that = this;
-    wx.getUserInfo({
-      success: function(res) {
-        console.log(res);
-
-        var userInfo = res.userInfo;
-        console.log(userInfo);
-        var nickName = userInfo.nickName;
-        var avatarUrl = userInfo.avatarUrl;
-        var gender = userInfo.gender; // 性别 0：未知、1：男、2：女
-        var province = userInfo.province;
-        var city = userInfo.city;
-        var country = userInfo.country;
-        if (gender == 1) {
-          gender = '男';
-        } else if (gender == 2) {
-          gender = '女';
-        } else {
-          gender = '未知';
-        }
-        that.setData({
-          nickName: nickName,
-          avatarUrl: avatarUrl,
-          gender: gender,
-          country: country,
-          province: province,
-          city: city,
-          editNickName: nickName,
-          editGender: gender,
-          editProvince: province,
-          editCity: city
-        });
-      }
-    });
+    this.loadUserInfo();
   },
 
   onShow() {
@@ -92,6 +59,7 @@ Page({
   },
 
   onSelect() {
+    this.saveUserInfo();
     this.setData({
       nickName: this.data.editNickName,
       gender: this.data.editGender,
@@ -115,5 +83,74 @@ Page({
 
   onCityChange(event) {
     this.setData({ editCity: event.detail });
+  },
+
+  saveUserInfo() {
+    const userInfo = {
+      nickName: this.data.editNickName,
+      avatarUrl: this.data.avatarUrl,
+      gender: this.data.editGender,
+      province: this.data.editProvince,
+      city: this.data.editCity,
+      country: this.data.country
+    };
+    wx.setStorageSync('userInfo', userInfo);
+  },
+
+  loadUserInfo() {
+    const userInfo = wx.getStorageSync('userInfo');
+    if (userInfo) {
+      this.setData({
+        nickName: userInfo.nickName,
+        avatarUrl: userInfo.avatarUrl,
+        gender: userInfo.gender,
+        country: userInfo.country,
+        province: userInfo.province,
+        city: userInfo.city,
+        editNickName: userInfo.nickName,
+        editGender: userInfo.gender,
+        editProvince: userInfo.province,
+        editCity: userInfo.city
+      });
+    } else {
+      this.getUserInfoFromAPI();
+    }
+  },
+
+  getUserInfoFromAPI() {
+    var that = this;
+    wx.getUserInfo({
+      success: function(res) {
+        console.log(res);
+
+        var userInfo = res.userInfo;
+        console.log(userInfo);
+        var nickName = userInfo.nickName;
+        var avatarUrl = userInfo.avatarUrl;
+        var gender = userInfo.gender; // 性别 0：未知、1：男、2：女
+        var province = userInfo.province;
+        var city = userInfo.city;
+        var country = userInfo.country;
+        if (gender == 1) {
+          gender = '男';
+        } else if (gender == 2) {
+          gender = '女';
+        } else {
+          gender = '未知';
+        }
+        that.setData({
+          nickName: nickName,
+          avatarUrl: avatarUrl,
+          gender: gender,
+          country: country,
+          province: province,
+          city: city,
+          editNickName: nickName,
+          editGender: gender,
+          editProvince: province,
+          editCity: city
+        });
+      }
+    });
   }
 });
